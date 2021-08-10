0226ad5bb780fd2aaa7b536e0c2f366d5f8a5430
Peter Winskill <p.winskill@imperial.ac.uk>
2021-08-10 10:46:25 +0100

    Cost effectivness comparison tables

--- analysis/04_combined_results.R
+++ analysis/04_combined_results.R
@@ -83,12 +83,37 @@ imperial_impact <- readRDS("analysis/data/derived_data/impact.RDS") %>%
 load(paste0(dropbox_loc, "2021_predictions/Data_July2021/SwissTPH/SwissTPH_transmissionCovSen_20210709_Cov_3d_80_4d_64_min.Rdata"))
 figure_min_cod <- make_main_figure(imperial_impact, Data_SwissTPH)
 ggsave("analysis/combined_output/figure_min_cod.png", figure_min_cod, height = 6, width = 10)
+
+# Main table: min Cod
+swiss_table_min_cod <- read.csv(paste0(dropbox_loc, "/2021_Figures_SwissTPH/report_July2021_Cov80/summariesForReport2021/summaryTransmission_80cov_minCOD.csv")) %>%
+  filter(vaccine == "with_booster", outcome != "propDeathsAverted", outcome != "severeCasesper100kvaccaverted") %>%
+  mutate(SwissTPH = paste(med_10to50, range_10to50)) %>%
+  select(outcome, SwissTPH)
+imperial_table_min_cod <- read.csv("analysis/output/imperial_impact_estimate_2021.csv") %>%
+  filter(rtss_coverage == 0.5, delivery_cost == 0.96) %>% 
+  mutate(Imperial = paste(med_10to50, range_10to50)) %>%
+  select(outcome, Imperial)
+table_min_cod <- left_join(swiss_table_min_cod, imperial_table_min_cod, by = "outcome")
+write.csv(table_min_cod, "analysis/combined_output/main_table_min_cod.csv")
+
 # Main figure: max Cod
 imperial_impact <- readRDS("analysis/data/derived_data/impact.RDS") %>%
   filter(rtss_coverage == 0.8, delivery_cost == 2.67)
 load(paste0(dropbox_loc, "2021_predictions/Data_July2021/SwissTPH/SwissTPH_transmissionCovSen_20210709_Cov_3d_80_4d_64_max.Rdata"))
 figure_max_cod <- make_main_figure(imperial_impact, Data_SwissTPH)
 ggsave("analysis/combined_output/figure_max_cod.png", figure_max_cod, height = 6, width = 10)
+
+# Main table: max Cod
+swiss_table_max_cod <- read.csv(paste0(dropbox_loc, "/2021_Figures_SwissTPH/report_July2021_Cov80/summariesForReport2021/summaryTransmission_80cov_maxCOD.csv")) %>%
+  filter(vaccine == "with_booster", outcome != "propDeathsAverted", outcome != "severeCasesper100kvaccaverted") %>%
+  mutate(SwissTPH = paste(med_10to50, range_10to50)) %>%
+  select(outcome, SwissTPH)
+imperial_table_max_cod <- read.csv("analysis/output/imperial_impact_estimate_2021.csv") %>%
+  filter(rtss_coverage == 0.5, delivery_cost == 2.67) %>% 
+  mutate(Imperial = paste(med_10to50, range_10to50)) %>%
+  select(outcome, Imperial)
+table_max_cod <- left_join(swiss_table_max_cod, imperial_table_max_cod, by = "outcome")
+write.csv(table_max_cod, "analysis/combined_output/main_table_max_cod.csv")
 ################################################################################
 
 ### Appex table for comparison #################################################
@@ -104,4 +129,25 @@ penny_et_al_table <- read.csv("analysis/data/raw_data/penny_et_al_table2.csv")
 table_comparison <- left_join(swiss_table_comparison, imperial_table_comparison, by = "outcome") %>%
   left_join(penny_et_al_table, by = "outcome")
 write.csv(table_comparison, "analysis/combined_output/table_comparison.csv")
+################################################################################
+
+### CE comparison table ########################################################
+cov80codmean <- read.csv("analysis/combined_output/main_table.csv") %>%
+  mutate(scenario = "cov80codmean")
+cov50codmean <- read.csv("analysis/combined_output/main_table_low_coverage.csv") %>%
+  mutate(scenario = "cov50codmean")
+cov90codmean <- read.csv("analysis/combined_output/main_table_high_coverage.csv") %>%
+  mutate(scenario = "cov90codmean")
+cov80codmin <- read.csv("analysis/combined_output/main_table_min_cod.csv") %>%
+  mutate(scenario = "cov80codmin")
+cov80codmax <- read.csv("analysis/combined_output/main_table_max_cod.csv") %>%
+  mutate(scenario = "cov80codmax")
+
+ce_compare <- bind_rows(cov80codmean, cov50codmean, cov90codmean, cov80codmin, cov80codmax) %>%
+  select(-X) %>%
+  filter(grepl("costper", outcome)) %>%
+  pivot_longer(-c(outcome, scenario), names_to = "Group", values_to =  "y") %>%
+  pivot_wider(names_from = scenario, values_from = y) %>%
+  arrange(Group)
+write.csv(ce_compare, "analysis/combined_output/cea_comparsion.csv")
 ################################################################################
